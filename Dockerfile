e371e6cbd46c9c5bb43ab59956e90e30587b26b2
Kishan <kishan@hathiwala.net>
2023-06-07 19:46:26 +1000

    Init commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+#See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+FROM mcr.microsoft.com/dotnet/aspnet:7.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+
+COPY ["TorServer/TorServer.csproj", "TorServer/"]
+RUN dotnet restore "TorServer/TorServer.csproj"
+
+COPY . .
+WORKDIR "/src/TorServer"
+RUN dotnet build "TorServer.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "TorServer.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+
+RUN apt update && apt upgrade -y && apt install tor -y
+
+EXPOSE 80
+EXPOSE 443
+EXPOSE 9050
+
+ENTRYPOINT ["dotnet", "TorServer.dll"]
---
+++ TorServer/Services/TorServer.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace TorServer.Services
+{
+    public class TorServer : BackgroundService
+    {
+        private readonly ILogger<TorServer> _logger;
+
+        public TorServer(ILogger<TorServer> logger) => _logger = logger;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("TorServer is running");
+                try
+                {
+                    var process = new Process();
+                    process.StartInfo.FileName = "tor";
+                    process.StartInfo.Arguments = "-f /etc/tor/torrc";
+
+                    process.Start();
+                    await process.WaitForExitAsync(stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError("{error}", ex);
+                }
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
